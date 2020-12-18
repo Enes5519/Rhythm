5bce391dea83426aea5bf9c0b9b45012876354cc
Enes Yıldırım <enes5519@gmail.com>
2020-12-18 17:50:50 +0300

    Add Ads

--- app/src/main/java/com/enes5519/rhythm/SearchActivity.kt
+++ app/src/main/java/com/enes5519/rhythm/SearchActivity.kt
@@ -6,12 +6,20 @@ import android.widget.EditText
 import android.widget.ImageView
 import androidx.core.widget.doAfterTextChanged
 import androidx.navigation.findNavController
+import com.google.android.gms.ads.AdRequest
+import com.google.android.gms.ads.AdView
+import com.google.android.gms.ads.MobileAds
 
 class SearchActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_search)
 
+        initViews()
+        initAd()
+    }
+
+    private fun initViews() {
         val et = findViewById<EditText>(R.id.search_et).apply {
             doAfterTextChanged {
                 val nav = findNavController(R.id.nav_host_fragment)
@@ -27,5 +35,14 @@ class SearchActivity : AppCompatActivity() {
         }
 
         findViewById<ImageView>(R.id.back_button).setOnClickListener { finish() }
+    }
+
+    private fun initAd(){
+        MobileAds.initialize(this) {}
+
+        findViewById<AdView>(R.id.adView).apply {
+            val adRequest = AdRequest.Builder().build()
+            loadAd(adRequest)
+        }
     }
 }
